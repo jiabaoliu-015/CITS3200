30e1cb1dc0101773e5c1efa94be01d50e727345f
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-08-25 09:30:31 +0800

    update main.py to use menu system

--- src/adeval/main.py
+++ src/adeval/main.py
@@ -1,32 +1,23 @@
-import time
+from adeval.console import console
+from adeval.menus.base_menu import Menu
+from adeval.menus.download_menu import DownloadMenu
+from adeval.menus.main_menu import MainMenu
 
-from console import console
-from rich.progress import track
-from rich.prompt import Prompt
-from rich.tree import Tree
+MENUS: dict[str, Menu] = {"main_menu": MainMenu(), "download_menu": DownloadMenu()}
 
-console.print([1, 2, 3])
-console.print("[blue underline]Looks like a link")
-# console.print(locals())
-console.print("FOO", style="white on blue")
 
-for i in track(range(20), description="Processing..."):
-    time.sleep(1)  # Simulate work being done
-
-# console.print(list(SPINNERS.keys()))
-
-pizza_name = Prompt.ask("Enter your pizza name")
-
-with console.status(f"Making {pizza_name}", spinner="shark") as status:
-    time.sleep(1)
-    status.update("Adding Toppings")
-    time.sleep(1)
-    status.update("Adding Cheese")
-    time.sleep(1)
-
-console.print("Done")
-
-tree = Tree("Rich Tree")
-tree.add("Apple")
-tree.add("Watermelon")
-console.print(tree)
+def run() -> None:
+    current_menu = "main_menu1"
+    try:
+        while current_menu is not None:
+            current_menu = MENUS[current_menu].run()
+    except KeyboardInterrupt:
+        console.print("[yellow]Interrupted. Exiting.[/yellow]")
+    except KeyError:
+        console.print("[red]Invalid Menu Name. Exiting.[/red]")
+        raise
+    except Exception:
+        console.print("[red]Something has gone wrong. Exiting.[/red]")
+        raise
+    else:
+        console.print("[green]Bye bye. Exiting.[/green]")
