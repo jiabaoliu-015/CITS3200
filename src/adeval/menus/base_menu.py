c8722ce7923128013e533c259dd4cd089a098482
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-08-26 17:52:41 +0800

    change menu name design

--- src/adeval/menus/base_menu.py
+++ src/adeval/menus/base_menu.py
@@ -6,13 +6,6 @@ class Menu(ABC):
     Base class for a menu screen. run() returns the name of the next menu to go to, or None to exit the app.
     """
 
-    @property
-    @abstractmethod
-    def menu_name(self) -> str:
-        """
-        Represents the menu name for linking in MENUS
-        """
-
     @abstractmethod
     def run(self) -> str | None:
         pass
