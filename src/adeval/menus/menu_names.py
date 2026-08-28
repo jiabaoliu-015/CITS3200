aa34ca34fd069526c50c336edc4ac8ea0b41a6a7
TimeAnomalyCode <aimanhakimin12345@gmail.com>
2026-08-28 20:41:49 +0800

    update comment for menu names

--- src/adeval/menus/menu_names.py
+++ src/adeval/menus/menu_names.py
@@ -1,7 +1,10 @@
-# Used to link MENUS
 from enum import Enum
 
 
 class MenuNames(Enum):
+    """
+    Used to link menus together
+    """
+
     MainMenu = "main_menu"
     DownloadMenu = "download_menu"
