88f2a10a560a255a8b6d4899ce3b613a118073ac
BotDurv <62335398+BotDurv@users.noreply.github.com>
2026-09-23 12:05:11 +0800

    Avoid unnecessary OpenPCDet dataset imports

--- tasks/perception_task/inference/pointpillars.py
+++ tasks/perception_task/inference/pointpillars.py
@@ -58,7 +58,7 @@ def run_pointpillars_inference(
         sys.path.insert(0, str(openpcdet_root))
 
     from pcdet.config import cfg, cfg_from_yaml_file
-    from pcdet.datasets import DatasetTemplate
+    from pcdet.datasets.dataset import DatasetTemplate
     from pcdet.models import build_network, load_data_to_gpu
     from pcdet.utils import common_utils
 
